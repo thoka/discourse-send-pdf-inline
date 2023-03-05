1040c19241b2791267e2a9e3cfca290a379818f4
Thomas Kalka <thomas.kalka@googlemail.com>
2023-03-05 13:02:54 +0100

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# name: send-pdf-inline
+# about: serve all PDFs inline
+# version: 0.1
+# authors: Thomas Kalka (toka)
+# url: https://github.com/thoka/discourse-send-pdf-inline
+
+module SendPdfInline
+  module UploadsControllerExtension
+    def send_file( path,options={} )
+      options[:disposition] = :inline if options.key?(:content_type) and options[:content_type]=="application/pdf"
+      super(path,options)
+    end
+  end
+end
+
+after_initialize do
+  ::UploadsController.prepend SendPdfInline::UploadsControllerExtension
+end
